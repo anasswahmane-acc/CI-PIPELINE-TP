2f0632638a88119a3985270e4c9579d13c4fa105
Anas <12345678+username@users.noreply.github.com>
2026-03-02 15:26:59 +0000

    N12555

--- app/user_manager.py
+++ app/user_manager.py
@@ -1,22 +1,27 @@
+"""Simple in-memory user manager used by tests."""
+
+
 class UserManager:
+    """Manage users in memory keyed by unique username."""
 
     def __init__(self):
-        self._users = []
+        self._users = {}
 
-    def add_user(self, username):
-        if not username:
-            raise ValueError("Username cannot be empty")
+    def add_user(self, username, user_data=None):
         if username in self._users:
             raise ValueError("User already exists")
-        self._users.append(username)
+        self._users[username] = user_data
+
+    def get_user(self, username):
+        return self._users.get(username)
 
     def remove_user(self, username):
         if username not in self._users:
             raise ValueError("User not found")
-        self._users.remove(username)
+        return self._users.pop(username)
+
+    def list_users(self):
+        return dict(self._users)
 
     def count_users(self):
-        return len(self._users)
-    
-    def count_total_users(self):
         return len(self._users)
