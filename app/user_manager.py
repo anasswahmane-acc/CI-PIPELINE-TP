5555e9c844850581310d45c9a1daf79486875c9c
anasswahmane-acc <anasswahmane@gmail.com>
2026-03-02 14:36:01 +0000

    Implement UserManager class for user management

--- app/user_manager.py
+++ app/user_manager.py
@@ -1,1 +1,12 @@
-# hgggg
+class UserManager:
+    def __init__(self):
+        self.users = []
+
+    def add_user(self, name):
+        self.users.append(name)
+
+    def remove_user(self, name):
+        self.users.remove(name)
+
+    def count_users(self):
+        return len(self.users)
